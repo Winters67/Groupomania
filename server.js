d85586af684574198fdf748b108bc15ad98de9f3
Winters67 <emmanuel.vbg@gmail.com>
2022-06-12 10:55:45 +0200

    Logique routing

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
-require("dotenv").config({ path: "./config/.env" });
-require("./config/db");
 const app = express();
 
+const userRoutes = require("./routes/user.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+
+app.use(express.json());
+
+// routes
+app.use("/api/user", userRoutes);
+
+//server
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
