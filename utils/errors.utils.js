af7e28c587dab9ade2cdc422151a733ba8c61f14
Winters67 <emmanuel.vbg@gmail.com>
2022-06-13 16:45:16 +0200

    mise en place des routes post

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -9,7 +9,20 @@ module.exports.signUpErrors = (err) => {
   if (err.message.includes("password"))
     errors.password = "Le mot de passe doit faire 6 caractères minimum";
 
-  if (err.code === 11000) errors.email = "Cet email est déjà enregisté ";
+  if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("pseudo"))
+    errors.pseudo = "Ce pseudo  est déjà pris ";
+  if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("email"))
+    errors.email = "Cet email est déjà enregisté ";
+
+  return errors;
+};
+module.exports.signInErrors = (err) => {
+  let errors = { email: "", password: "" };
+
+  if (err.message.includes("email")) errors.email = "Email inconnu";
+
+  if (err.message.includes("password"))
+    errors.password = "le mot de passe ne correspond pas";
 
   return errors;
 };
