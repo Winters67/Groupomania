c3d84ef082f701ca96006a4258e287302513d0ca
Winters67 <emmanuel.vbg@gmail.com>
2022-06-24 10:57:14 +0200

    popup like

--- client/src/components/Post/Card.jsx
+++ client/src/components/Post/Card.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { isEmpty } from '../Utils';
+import { dateParser, isEmpty } from '../Utils';
+import FollowHandler from '../Profil/FollowHandlers';
+import LikeButton from './LikeButton';
 
 const Card = ({ post }) => {
     const [isLoading, setIsLoading] = useState(true);
@@ -30,8 +32,56 @@ const Card = ({ post }) => {
                                 }).join('')
 
                             }
-                            alt=""
+                            alt="poster-pic"
                         />
+                    </div>
+                    <div className="card-right">
+                        <div className="card-header">
+                            <div className="pseudo">
+                                <h3>
+                                    {
+                                        !isEmpty(
+                                            usersData[0]) &&
+                                        usersData.map((user) => {
+                                            if (user._id === post.posterId) return user.pseudo;
+                                        })
+                                    }
+                                </h3>
+                                {post.posterId !== userData._id && (
+                                    <FollowHandler idToFollow={post.posterId} type={"card"} />
+                                )}
+                            </div>
+                            <span>{dateParser(post.createdAt)} </span>
+                        </div>
+                        <p>
+                            {post.message}
+                        </p>
+                        {post.picture && (
+                            <img src={post.picture} alt="card-pic" className='card-pic' />
+                        )}
+                        {post.video && (
+                            <iframe
+                                width="500"
+                                height="300"
+                                src={post.video}
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                title={post._id}
+                            ></iframe>
+                        )}
+                        <div className="card-footer">
+                            <div className="comment-icon">
+                                <img
+                                    src="./img/icons/message1.svg"
+                                    alt="comment"
+                                />
+                                <span>{post.comments.length}</span>
+                            </div>
+                            <LikeButton post={post} />
+                            <img src="./img/icons/share.svg" alt="share" />
+                        </div>
+
                     </div>
                 </>
             )}
