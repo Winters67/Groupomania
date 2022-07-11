4769153a4b3bd7110c1d59ffd9a62e470b3a42fd
Winters67 <emmanuel.vbg@gmail.com>
2022-07-11 18:19:13 +0200

    test

--- client/src/components/Profil/Uploadimg.jsx
+++ client/src/components/Profil/Uploadimg.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { uploadPicture } from "../../actions/user.actions";
 
+// uploadinf
 const UploadImg = () => {
     const [file, setFile] = useState();
     const dispatch = useDispatch();
